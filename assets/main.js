cb14db0bb577ea25a3ce13dc4a73e3d187504731
MrNaughtZero <neil.naught@icloud.com>
2021-01-15 17:17:15 +0000

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+function openMenu(){
+    document.getElementsByClassName('mobile-menu-links')[0].classList.add('mobile-menu-animation');
+    document.getElementsByClassName('mobile-menu-links')[0].style.top = '0';
+    document.getElementsByClassName('mobile-menu')[0].onclick = hideMenu;
+}
+
+function hideMenu(){
+    document.getElementsByClassName('mobile-menu-links')[0].style.top = '-400px';
+    document.getElementsByClassName('mobile-menu')[0].onclick = openMenu;
+}
+
+var header = document.getElementById("main-links");
+var btns = header.getElementsByClassName("nav-item");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace("active", "");
+  this.className += " active";
+  });
+}
